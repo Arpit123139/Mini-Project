bf7494985ed5703b2a5119b2e0fb4f734bffa71e
Arpit123139 <arpitgajya2001@gmail.com>
2023-04-29 19:38:27 +0530

    second

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -13,13 +13,42 @@ exports.addRecord=BigPromise(async(req,res)=>{
     const id=decode.id;
 
     const {report}=req.body;
+    // console.log("Report!!!!!!!! "+report)
+    const recordExsist=await Record.findOne({user:id})
 
-    const report1=await Record.create({
-        user:id,
-        report:report
-    })
+    if(recordExsist){
+        // console.log("Record Exsist")
+        const record=recordExsist.report
 
-    res.status(200).json(report1)
+        // console.log("Record "+record);
+        const arr=[]
+        for(let i=0;i<record.length;i++)
+            arr.push(record[i]);
+        for(let j=0;j<report.length;j++)
+            arr.push(report[j])
+        
+            // console.log("Array "+arr)
+        const newData={
+            user:id,
+            report:arr
+        }
+
+        await Record.findByIdAndUpdate(recordExsist._id,newData)
+
+        res.status(200).json({
+            success:true
+        })
+    }
+    else{
+        const report1=await Record.create({
+            user:id,
+            report:report
+        })
+        res.status(200).json(report1)
+    }
+   
+
+    
 
 
     
